bc72dd2a84602a055651d15e875c4faeb8aa8156
Damianos Sotirakis <dansotirakis18@gmail.com>
2022-05-07 18:28:31 -0300

    IU-3-sqs-app-elbow-r02 : init react elbow-app

---
+++ app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Elbow</p>
+        <a className="App-link" href="https://github.com/dansotirakis/lab-java-sqs-elbow"
+           target="_blank" rel="noopener noreferrer" >
+          Repository APP
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
