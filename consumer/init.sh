c2cbc9b10c049314158f6e947aa527ebedbf2772
Damianos Sotirakis <dansotirakis18@gmail.com>
2022-07-18 03:02:30 -0300

    IU-3-sqs-app-elbow-r02 : update version at engine dockerized run

--- consumer/init.sh
+++ consumer/init.sh
@@ -2,5 +2,4 @@
 aws configure set aws_access_key_id "$AWS_SECRET_ACCESS_KEY"
 aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
 aws configure set region "$AWS_REGION"
-aws --endpoint-url="$AWS_URL" sqs create-queue --queue-name "$QUEUE_NAME" --attributes FifoQueue=true
-java -jar -Dspring.profiles.active=local /java-application.jar
+aws --endpoint-url="$AWS_URL" sqs list-queues
